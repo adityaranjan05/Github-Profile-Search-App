ec3423eae3906fc54bb31f85ca74d17d87a57268
Aditya Ranjan Sinha <aditya.ranjan.cs@gmail.com>
2026-05-27 13:14:43 +0530

    fixed some ui

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 "use strict";
 
+const api = "https://api.github.com/users/";
+const input = document.querySelector("#idSearch");
+const card = document.querySelector("#card");
+
+const searchId = async () => {
+    const username = input.value;
+    try {
+        
+    }
+    catch (error) {
+        console.log(error)
+        card.innerHTML = "<div>Error loading data....</div>"    ;
+    }
+}
+
+input.addEventListener("keypress", (e) => {
+    if(e.key === "Enter"){
+        searchID();
+    }
+});
