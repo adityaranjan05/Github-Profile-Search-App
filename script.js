d0e316f337e1701e6d2743fd68125f3ddd9fd6ce
Aditya Ranjan Sinha <aditya.ranjan.cs@gmail.com>
2026-05-27 14:41:21 +0530

    Did some final changes

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ const api = "https://api.github.com/users/";
 const input = document.querySelector("#idSearch");
 const card = document.querySelector("#card");
 
-const searchId = async () => {
+const searchID = async () => {
     const username = input.value;
     try {
-        
+        const response = await fetch(api + username);
+        const data = await response.json();
+        console.log(data);
+        card.innerHTML = 
+        `
+            <div>
+                <img class="avatar" src="${data.avatar_url}" alt="Florin pop"/>
+            </div>
+            <div class="user_info">
+                <h2>Name : ${data.name}</h2>
+                <p>Bio : ${data.bio}</p>
+                <ul>
+                <li>Followers : ${data.followers}</li>
+                <li>Following : ${data.following}</li>
+                <li>Repos : ${data.public_repos}</li>
+                </ul>
+            </div>
+        `
     }
     catch (error) {
         console.log(error)
@@ -15,7 +32,7 @@ const searchId = async () => {
     }
 }
 
-input.addEventListener("keypress", (e) => {
+input.addEventListener("keydown", (e) => {
     if(e.key === "Enter"){
         searchID();
     }
